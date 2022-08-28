f7cffc0d7620785fa1fae8c32e4b5f1dedac5706
T81oub <93501885+T81oub@users.noreply.github.com>
2022-08-29 00:53:25 +0100

    v5

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+
+
+/*=============== HOME SWIPER ===============*/
+let homeSwiper = new Swiper(".home-swiper", {
+    spaceBetween: 30,
+    loop: 'true',
+    
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+})
+
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+})
+
+sr.reveal(`.home-swiper`)
+
